ec3408ffc67ab61822394815e47f0dfe0a211324
Lexxyy1 <denalhutabarat16@gmail.com>
2026-02-01 22:44:10 +0700

    lexxy

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$conn = mysqli_connect("localhost", "root", "", "user_system");
-if (!$conn) { die("Koneksi Database Gagal!"); }
-date_default_timezone_set('Asia/Jakarta');
-?>
